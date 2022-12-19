4b127ad8cb83dc42668373a5acd705b33dc84daa
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2022-12-20 01:18:42 +0700

    update files

--- js/viewHistory.js
+++ js/viewHistory.js
@@ -28,30 +28,5 @@ export const renderViewHistory = () => {
         </div>
         `;
     });
-    // return `
-    // <div id="history-header-top">
-    //     <h2 id="history-header-title">Recent Cards</h2>
-    // </div>
-    // <div class="recent-card-container">
-    //     <div class ="recent-card" >
-    //         <div class="recent-card-img"><img src="./assets/images/data/wand_1.png" alt="wand_1"></div>
-    //         <div class="recent-card-info">
-    //         <h3> Ace of Wand </h3>
-    //         <p>Date viewed: 19/12/2022</p>
-    //         </div>
-    //         <div id="viewHistoryClickMe">
-    //             <img src="./assets/icons/icons8-info-68.png" alt="info-icon">
-    //         </div>
-    //     </div>
-    //     <div class ="recent-card" >
-    //         <div>17/12/2022 - La bai 1</div>
-    //         <div>abc</div>
-    //         <div id="viewHistoryClickMe">
-    //             More info
-    //             <img src="./assets/icons/icons8-info-68.png" alt="info-icon">
-    //         </div>
-    //     </div>
-    // </div>
-    // `;
     return content + '</div>';
 };
